6748000db846281cabd6af156139af028165e13d
William Pardis <wpardis@whoi.edu>
2022-04-06 09:05:42 -0400

    combination of import and calibrationDeve creates .mat structs that will be used to calibrate NOPP

---
+++ guaymas_data_analysis/data_processing/nopp/calibrationDevelopment.m
@@ -0,0 +1,59 @@
+%% working on calibration
+clear
+
+%% import
+% relative air calibration file path
+path = "data/cal/air";
+file = "N2cal.txt";
+filepath = fullfile(path,file);
+% import cal for NOPP1
+[laser, aux] = importNopp(filepath);
+%% plotting
+
+% reference gas values [PPM]
+ref = [0 4 10 100 1000 10000];
+
+% plot time series
+figure('units','normalized','outerposition',[0 0 1 1])  % full screen
+    subplot(2,1,1)
+        plot(movmean(laser.R,10));hold on;yyaxis right;plot(movmean(laser.F,10))
+
+
+if(exist('fPoints','var')==0)
+    % user select equilibration input
+    for i=1:length(ref)
+    
+        [x(:,i),~] = ginput(2)
+        T=x(1,i):x(2,i);
+        T=round(T);
+        
+        subplot(2,1,1);hold on
+            yyaxis left;plot(T,laser.R(T),'k');yyaxis right;plot(T,laser.F(T),'k')
+    
+        % calculate average
+        fPoints(i) = mean(laser.F(T),'omitnan');
+        rPoints(i) = mean(laser.R(T),'omitnan');
+
+        subplot(2,2,3);hold on;bubblechart(fPoints(i),log10(ref(i)),1,"black")
+        subplot(2,2,4);hold on;bubblechart(rPoints(i),log10(ref(i)),1,"black")
+    
+    end
+
+else
+
+    subplot(2,2,3);hold on;bubblechart(fPoints,log10(ref),1,"black")
+    subplot(2,2,4);hold on;bubblechart(rPoints,log10(ref),1,"black")
+
+end 
+
+%% Organize calibration into structure
+cal.ref = ref;
+cal.rPoints = rPoints;
+cal.fPoints= fPoints;
+cal.aux = aux;
+cal.laser = laser;
+
+%% save calibration in origonal directory
+tempStr = split(file,'.');
+filepath = fullfile(path,strcat(tempStr(1),".mat"));
+save(filepath, '-struct', 'cal');
--- guaymas_data_analysis/data_processing/nopp/importNopp.m
+++ guaymas_data_analysis/data_processing/nopp/importNopp.m
@@ -1,20 +1,29 @@
 function [laser,aux] = importNopp(filepath)
+    
+
     %% importing NOPP datafile to Matlab
+    
     % if file path is not defined exsplicitly prompt user to select
     if(exist('filepath','var')==0)
         [file,path] = uigetfile('*.txt');
         filepath    = fullfile(path,file);
     end
+    
     % read filepath as table
     data = readtable(filepath);
+    
+   
     %% parse laser and auxilary data structures
-    %time format
+    
+    % time format
     infmt = 'yyyyMMdd''T''HHmmss.SSS';
+    
     % laser: time(t), temperature(T), ringdown(R), fundamental(F)
     laser = data(:,{'Var1','Var2','Var3','Var4'});                      
     laser.Properties.VariableNames = {'t','T','R','F'};
-    laser.Properties.VariableUnits = {'datetime' 'Celsius' 'PSI' ''}
+    laser.Properties.VariableUnits = {'datetime' 'Celsius' 'PSI' ''};
     laser.t = datetime(laser.t,'InputFormat',infmt);    %datetime conversion
+    
     % auxillary: time(t), cell-pressure(cP), cell-temperature(cT),
     %            housing-pressure(hP), housting-temperature(hT),
     %            housting-relative-humidity(RH), power-states(states)
@@ -22,4 +31,9 @@ function [laser,aux] = importNopp(filepath)
     aux.Properties.VariableNames = {'t','cT','cP','hT','hP','RH','states'};
     aux.Properties.VariableUnits = {'datetime' 'Celsius' 'PSI' 'Celcius' 'PSI' '%' ''};
     aux.t = datetime(aux.t,'InputFormat',infmt);    %datetime conversion
+
+    % filtering outliers
+    laser.F(laser.F>1)=nan;
+    laser.R(laser.R>5)=nan;
+
 end
